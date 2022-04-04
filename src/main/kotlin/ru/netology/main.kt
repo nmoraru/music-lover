a34657c5d3cb6dfab31a9353ecbd37695744e6ab
Nikita Moraru <nomoraru@gmail.com>
2022-04-04 17:48:57 +0300

    initial commit

---
+++ src/main/kotlin/ru/netology/main.kt
@@ -0,0 +1,21 @@
+package ru.netology
+
+fun main() {
+    val regularCustomer: Boolean = true
+
+    val oldAmount: Int = 10001_00
+    val amount: Int = 100_00
+
+    val standardSale: Int = 100_00
+    val topSale: Double = 0.05
+    val regularCustomerSale: Double = 0.01
+
+    val minAmountForStandardSale = 1001_00
+    val maxAmountForStandardSale = 10000_00
+    val minAmountForTopSale = 10000_01
+
+    val amountAfterBaseSale = if (oldAmount >= minAmountForStandardSale && oldAmount <= maxAmountForStandardSale) amount - standardSale else if (oldAmount >= minAmountForTopSale) amount * (1 - topSale) else amount
+    val amountAfterRegularCustomerSale = if (regularCustomer === true) amountAfterBaseSale.toDouble() * (1 - regularCustomerSale) else amountAfterBaseSale
+
+    println(amountAfterRegularCustomerSale.toInt())
+}
